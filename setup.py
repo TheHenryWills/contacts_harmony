d99887550e4c437f914c4a196ce5aadd88c4052c
Henry Wills <124279910+TheHenryWills@users.noreply.github.com>
2023-02-14 20:09:43 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="contacts_harmony",
+    version="0.0.1",
+    author="Hnery Wills",
+    author_email="henry@debutwith.com",
+    description="A Python library to normalize and validate email addresses and phone numbers entered into web forms.",
+    url="https://github.com/TheHenryWills/contacts_harmony/tree/main/contacts_harmony",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
