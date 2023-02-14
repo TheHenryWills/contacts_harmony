8e803de8507d1cb49af8c5fda9011dcc3d3f5b80
Tobias Herrmann <tobias@twinfive.com>
2023-02-14 20:36:01 +0100

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 import setuptools
 
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="contacts_harmony",
     version="0.0.1",
     author="Henry Wills",
     author_email="henry@debutwith.com",
     description="A Python library to normalize and validate email addresses and phone numbers entered into web forms.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     url="https://github.com/TheHenryWills/contacts_harmony/tree/main/contacts_harmony",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_dir = {"": "contacts_harmony"},
+    packages = setuptools.find_packages(where="contacts_harmony"),
     python_requires='>=3.6',
 )
